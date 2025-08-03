0dea0ee83b23512a4a0bb827dbbee7ba1b15f145
kooooichi24 <ex.kouichi1224@gmail.com>
2025-08-04 00:56:06 +0900

    refactor: Define DEFAULT_QUERIES_OPTIONS

--- app/plugins/vue-query.ts
+++ app/plugins/vue-query.ts
@@ -1,15 +1,17 @@
 import { VueQueryPlugin, QueryClient } from '@tanstack/vue-query'
 import { defineNuxtPlugin } from 'nuxt/app'
 
+export const DEFAULT_QUERIES_OPTIONS = {
+  staleTime: 1000 * 60 * 5,
+  retry: 1,
+  refetchOnWindowFocus: false,
+  refetchOnMount: false,
+}
+
 export default defineNuxtPlugin((nuxtApp) => {
   const queryClient = new QueryClient({
     defaultOptions: {
-      queries: {
-        staleTime: 1000 * 60 * 5,
-        retry: 1,
-        refetchOnWindowFocus: false,
-        refetchOnMount: false,
-      },
+      queries: DEFAULT_QUERIES_OPTIONS,
     },
   })
 
