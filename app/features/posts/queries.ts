5cc70002e3f9f5074fededc67124c3cb785aa65f
kooooichi24 <ex.kouichi1224@gmail.com>
2025-09-05 16:31:41 +0900

    feat: Add queries at posts

--- app/features/posts/queries.ts
+++ app/features/posts/queries.ts
@@ -3,6 +3,8 @@ import { createQueryKeys } from "@lukemorales/query-key-factory";
 
 export const postQueryKeys = createQueryKeys('posts', {
   list: null,
+  listByUserId: (userId: number) => [userId],
+  listByUserIds: (userIds: number[]) => [userIds],
   get: (id: number) => [id],
 })
 
@@ -10,6 +12,20 @@ export async function useListPosts() {
   return useQuery({
     queryKey: postQueryKeys.list.queryKey,
     queryFn: () => fetch('https://jsonplaceholder.typicode.com/posts'),
+  })
+}
+
+export async function useListPostsByUserId(userId: number) {
+  return useQuery({
+    queryKey: postQueryKeys.listByUserId(userId).queryKey,
+    queryFn: () => fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`),
+  })
+}
+
+export async function useListPostsByUserIds(userIds: number[]) {
+  return useQuery({
+    queryKey: postQueryKeys.listByUserIds(userIds).queryKey,
+    queryFn: () => fetch(`https://jsonplaceholder.typicode.com/posts?${userIds.map(userId => `userId=${userId}`).join('&')}`),
   })
 }
 
