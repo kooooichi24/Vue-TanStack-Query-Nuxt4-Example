7023cc737c5b643d022e9f6c6e6e642aa578cdce
kooooichi24 <ex.kouichi1224@gmail.com>
2025-09-05 16:05:52 +0900

    refactor: Rename query keys

--- app/features/posts/queries.ts
+++ app/features/posts/queries.ts
@@ -1,23 +1,21 @@
 import { useQuery } from "@tanstack/vue-query";
 import { createQueryKeys } from "@lukemorales/query-key-factory";
 
-const postKeys = createQueryKeys('posts', {
+export const postQueryKeys = createQueryKeys('posts', {
   list: null,
   get: (id: string) => [id],
 })
 
-export const postQueryKeys = postKeys
-
 export async function useListPosts() {
   return useQuery({
-    queryKey: postKeys.list.queryKey,
+    queryKey: postQueryKeys.list.queryKey,
     queryFn: () => fetch('https://jsonplaceholder.typicode.com/posts'),
   })
 }
 
 export async function useGetPost(id: string) {
   return useQuery({
-    queryKey: postKeys.get(id).queryKey,
+    queryKey: postQueryKeys.get(id).queryKey,
     queryFn: () => fetch(`https://jsonplaceholder.typicode.com/posts/${id}`),
   })
 }
