c2a8dae3a1d5618a92d7f525a0974f8dd6e27399
kooooichi24 <ex.kouichi1224@gmail.com>
2025-09-05 16:32:35 +0900

    feat: Add queries at comments

--- app/features/comments/queries.ts
+++ app/features/comments/queries.ts
@@ -3,6 +3,8 @@ import { useQuery } from "@tanstack/vue-query"
 
 export const commentQueryKeys = createQueryKeys('comments', {
   list: null,
+  listByPostId: (postId: number) => [postId],
+  listByPostIds: (postIds: number[]) => [postIds],
   get: (id: number) => [id],
 })
 
@@ -10,6 +12,20 @@ export async function useListComments() {
   return useQuery({
     queryKey: commentQueryKeys.list.queryKey,
     queryFn: () => fetch('https://jsonplaceholder.typicode.com/comments'),
+  })
+}
+
+export async function useListCommentsByPostId(postId: number) {
+  return useQuery({
+    queryKey: commentQueryKeys.listByPostId(postId).queryKey,
+    queryFn: () => fetch(`https://jsonplaceholder.typicode.com/comments?postId=${postId}`),
+  })
+}
+
+export async function useListCommentsByPostIds(postIds: number[]) {
+  return useQuery({
+    queryKey: commentQueryKeys.listByPostIds(postIds).queryKey,
+    queryFn: () => fetch(`https://jsonplaceholder.typicode.com/comments?${postIds.map(postId => `postId=${postId}`).join('&')}`),
   })
 }
 
