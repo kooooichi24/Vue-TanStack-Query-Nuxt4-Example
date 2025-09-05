3a0d121eac06da94dd6970c333acaa675520cfa9
kooooichi24 <ex.kouichi1224@gmail.com>
2025-09-05 19:13:30 +0900

    feat: Improve type-safety response

--- app/features/posts/queries.ts
+++ app/features/posts/queries.ts
@@ -1,6 +1,13 @@
 import { useQuery } from "@tanstack/vue-query";
 import { createQueryKeys } from "@lukemorales/query-key-factory";
 
+type Post = {
+  id: number
+  title: string
+  body: string
+  userId: number
+}
+
 export const postQueryKeys = createQueryKeys('posts', {
   list: null,
   search: (filters: { userId?: number }) => [filters],
@@ -10,20 +17,30 @@ export const postQueryKeys = createQueryKeys('posts', {
 export async function useListPosts() {
   return useQuery({
     queryKey: postQueryKeys.list.queryKey,
-    queryFn: () => fetch('https://jsonplaceholder.typicode.com/posts'),
+    queryFn: async (): Promise<Post[]> => {
+      const response = await fetch('https://jsonplaceholder.typicode.com/posts')
+      return response.json()
+    },
   })
 }
 
 export async function useSearchPosts(filters: { userId?: number }) {
   return useQuery({
     queryKey: postQueryKeys.search(filters).queryKey,
-    queryFn: () => fetch(`https://jsonplaceholder.typicode.com/posts?userId=${filters.userId}`),
+    queryFn: async (): Promise<Post[]> => {
+      const response = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${filters.userId}`)
+      return response.json()
+    },
   })
 }
 
 export async function useGetPost(id: number) {
   return useQuery({
     queryKey: postQueryKeys.get(id).queryKey,
-    queryFn: () => fetch(`https://jsonplaceholder.typicode.com/posts/${id}`),
+    queryFn: async (): Promise<Post> => {
+      const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      return response.json()
+    },
   })
 }
+
