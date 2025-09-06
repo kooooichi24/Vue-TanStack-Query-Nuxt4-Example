6d9ea573e592f09dfc22b92d2043a81a51f1b23a
kooooichi24 <ex.kouichi1224@gmail.com>
2025-09-06 23:36:33 +0900

    feat: Create index.vue for posts page with data fetching and table display

---
+++ app/pages/posts/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="p-6">
+    <h1 class="text-2xl font-bold mb-4">Posts</h1>
+
+    <UTable
+      :data="data"
+      :columns="columns"
+      :loading="isLoading"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { TableColumn } from '@nuxt/ui';
+import { useListPosts, type Post } from '~/features/posts/queries'
+import { useListUsers, type User } from '~/features/users/queries';
+
+const { data: posts, isLoading: isLoadingPosts } = await useListPosts()
+const { data: users, isLoading: isLoadingUsers } = await useListUsers()
+
+const isLoading = computed(() => isLoadingPosts.value || isLoadingUsers.value)
+
+type TableData = Pick<Post, 'id' | 'title' | 'body'> & Pick<User, 'username'>
+
+const columns: TableColumn<TableData>[] = [
+  { accessorKey: 'id', header: 'ID' },
+  { accessorKey: 'title', header: 'Title' },
+  { accessorKey: 'username', header: 'Username' },
+  { accessorKey: 'body', header: 'Body' },
+]
+
+const data = computed(() => {
+  if (!posts.value || !users.value) return []
+
+  const userMap = new Map(users.value.map((user) => [user.id, user]))
+  return posts.value?.map((post) => ({
+    ...post,
+    username: userMap.get(post.userId)?.username ?? ""
+  }))
+})
+
+</script>
+
+<style scoped>
+</style>
