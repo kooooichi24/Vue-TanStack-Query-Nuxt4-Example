6dde2cbc035683fec86cc84108d3f4818fede85c
kooooichi24 <ex.kouichi1224@gmail.com>
2025-09-06 23:09:00 +0900

    feat: Add main CSS file and include Tailwind CSS and Nuxt UI imports

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/ui', '@nuxt/test-utils']
+  modules: ['@nuxt/eslint', '@nuxt/ui', '@nuxt/test-utils'],
+  css: ['~/assets/css/main.css']
 })
