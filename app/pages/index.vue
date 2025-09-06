f79dabe92df1fa15a13d485ee886fc469c197aed
kooooichi24 <ex.kouichi1224@gmail.com>
2025-09-06 23:09:21 +0900

    feat: Refactor app.vue to use NuxtPage component and create index.vue for navigation

---
+++ app/pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="p-6">
+    <NuxtLink 
+      to="/posts" 
+      class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+    >
+      Go to Posts
+    </NuxtLink>
+  </div>
+</template>
