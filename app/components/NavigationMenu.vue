6c3adbdf54f78b51a92239b4b270240a06f9283c
kooooichi24 <ex.kouichi1224@gmail.com>
2025-09-08 00:11:50 +0900

    feat: Add NavigationMenu component with links to Home, Posts, and Users pages

---
+++ app/components/NavigationMenu.vue
@@ -0,0 +1,27 @@
+<template>
+  <UNavigationMenu orientation="horizontal" :items="items" />
+</template>
+
+<script setup lang="ts">
+import type { NavigationMenuItem } from '@nuxt/ui'
+
+const items = ref<NavigationMenuItem[][]>([
+  [
+    {
+      label: 'Home Page',
+      type: 'link',
+      to: '/',
+    },
+    {
+      label: 'Posts Page',
+      type: 'link',
+      to: '/posts',
+    },
+    {
+      label: 'Users Page',
+      type: 'link',
+      to: '/users',
+    },
+  ]
+])
+</script>
