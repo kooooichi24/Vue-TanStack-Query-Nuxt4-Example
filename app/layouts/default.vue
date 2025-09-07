004cf9c483469e68a29884abcea2333db2767d3b
kooooichi24 <ex.kouichi1224@gmail.com>
2025-09-08 00:11:54 +0900

    feat: Create default layout with NavigationMenu component for consistent page structure

---
+++ app/layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <header class="border-b border-[var(--ui-border-accented)]">
+      <NavigationMenu />
+    </header>
+    <main>
+      <slot />
+    </main>
+  </div>
+</template>
