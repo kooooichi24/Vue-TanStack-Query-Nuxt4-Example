5c5d84887930ac222f2e0db64114f2b2031a9074
kooooichi24 <ex.kouichi1224@gmail.com>
2025-09-07 14:34:16 +0900

    refactor: Simplify post detail fetching by using dedicated query functions and improve loading state management

--- app/pages/posts/[id].vue
+++ app/pages/posts/[id].vue
@@ -6,44 +6,28 @@
       Loading...
     </div>
     
-    <div v-else-if="data" class="p-6 rounded-lg shadow">
-      <p>ID: {{ data.id }}</p>
-      <p>Title: {{ data.title }}</p>
-      <p>Body: {{ data.body }}</p>
-      <p>User ID: {{ data.userId }}</p>
-      <p>Username: {{ data.username }}</p>
+    <div v-else-if="post && user" class="p-6 rounded-lg shadow">
+      <p>ID: {{ post.id }}</p>
+      <p>Title: {{ post.title }}</p>
+      <p>Body: {{ post.body }}</p>
+      <p>User ID: {{ post.userId }}</p>
+      <p>Username: {{ user.username }}</p>
     </div>
   </div>
 </template>
 
 
 <script setup lang="ts">
-import { useQuery } from '@tanstack/vue-query'
-import { userQueryKeys } from '~/features/users/queries'
-import type { TableData } from './index.vue'
+import { useGetUser } from '~/features/users/queries'
+import { useGetPost } from '~/features/posts/queries'
 
 const route = useRoute()
 const postId = parseInt(route.params.id as string)
 
-async function useGetPostDetail(id: number) {
-  return useQuery({
-    queryKey: userQueryKeys.get(id).queryKey,
-    queryFn: async (): Promise<TableData> => {
-      const post = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`).then(res => res.json())
-      const user = await fetch(`https://jsonplaceholder.typicode.com/users/${post.userId}`).then(res => res.json())
-      return {
-        ...post,
-        username: user.username
-      }
-    },
-  })
-}
+const { data: post, isLoading: isLoadingPost } = await useGetPost(postId)
+const userId = computed(() => post.value?.userId)
+const { data: user, isLoading: isLoadingUser } = await useGetUser(userId)
 
-// const { data: post, isLoading: isLoadingPost } = await useGetPost(postId)
-// const { data: user, isLoading: isLoadingUser } = await useGetUser(post.value?.userId)
-
-// const isLoading = computed(() => isLoadingPost.value || isLoadingUser.value)
-
-const { data, isLoading } = await useGetPostDetail(postId)
+const isLoading = computed(() => isLoadingPost.value || isLoadingUser.value)
 
 </script>
