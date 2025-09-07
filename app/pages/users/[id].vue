cef8c32e07e130143d381fd1a5be988b4e4e124f
kooooichi24 <ex.kouichi1224@gmail.com>
2025-09-07 22:25:47 +0900

    feat: Create user detail page with data fetching and loading state management

---
+++ app/pages/users/[id].vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="p-6">
+    <h1 class="text-2xl font-bold mb-4">User {{ userId }}</h1>
+
+    <div v-if="isLoadingUser" class="text-gray-600">
+      Loading...
+    </div>
+    
+    <div v-else-if="user" class="p-6 rounded-lg shadow">
+      <p>ID: {{ user.id }}</p>
+      <p>Username: {{ user.username }}</p>
+      <p>Email: {{ user.email }}</p>
+      <p>Phone: {{ user.phone }}</p>
+      <p>Website: {{ user.website }}</p>
+      <p>Address: {{ user.address.street }}, {{ user.address.suite }}, {{ user.address.city }}, {{ user.address.zipcode }}</p>
+      <p>Company: {{ user.company.name }}, {{ user.company.catchPhrase }}, {{ user.company.bs }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useGetUser } from '~/features/users/queries'
+
+const route = useRoute()
+const userId = parseInt(route.params.id as string)
+
+const { data: user, isLoading: isLoadingUser } = await useGetUser(computed(() => userId))
+
+</script>
