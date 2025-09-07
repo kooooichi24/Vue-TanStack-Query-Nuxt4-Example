cd57b084d2acbf4966733a78db1dd8d4017fa1d8
kooooichi24 <ex.kouichi1224@gmail.com>
2025-09-08 00:35:32 +0900

    feat: Replace loading and content sections with UTable for displaying user-related posts, enhancing data presentation and interactivity

--- app/pages/users/[id].vue
+++ app/pages/users/[id].vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="p-6">
+  <div class="h-full flex flex-col p-6">
     <h1 class="text-2xl font-bold">User {{ userId }}</h1>
 
-    <div v-if="isLoadingUser" class="p-6 rounded-lg shadow">
+    <div v-if="isLoadingUser" class="py-6 rounded-lg shadow">
       <UCard>
         <div class="space-y-3">
           <USkeleton class="h-4 w-16" />
@@ -16,7 +16,7 @@
       </UCard>
     </div>
     
-    <div v-else-if="user" class="p-6 rounded-lg shadow">
+    <div v-else-if="user" class="py-6 rounded-lg shadow">
       <UCard>
         <p><strong>ID: </strong>{{ user.id }}</p>
         <p><strong>Username: </strong>{{ user.username }}</p>
@@ -30,31 +30,21 @@
 
     <h1 class="text-2xl font-bold">Related Posts</h1>
 
-    <div v-if="isLoadingPosts" class="p-6 rounded-lg shadow">
-      <UCard v-for="n in 3" :key="n" class="mb-4">
-        <div class="space-y-3">
-          <USkeleton class="h-4 w-12" />
-          <USkeleton class="h-4 w-3/4" />
-          <USkeleton class="h-4 w-full" />
-          <USkeleton class="h-4 w-16" />
-        </div>
-      </UCard>
-    </div>
-    
-    <div v-else-if="posts" class="p-6 rounded-lg shadow">
-      <UCard v-for="post in posts" :key="post.id" class="mb-4 cursor-pointer hover:bg-gray-800" @click="navigateTo(`/posts/${post.id}`)">
-        <p><strong>ID: </strong>{{ post.id }}</p>
-        <p><strong>Title: </strong>{{ post.title }}</p>
-        <p><strong>Body: </strong>{{ post.body }}</p>
-        <p><strong>User ID:</strong> {{ post.userId }}</p>
-      </UCard>
-    </div>
+    <UTable
+      :data="postsWithUsername"
+      :columns="columns"
+      :loading="isLoadingPosts"
+      class="mt-6 cursor-pointer border border-(--ui-border-accented) rounded-lg overflow-auto"
+      sticky
+      @select="(row: TableRow<TableData>) => navigateTo(`/posts/${row.original.id}`)"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
-import { useSearchPosts } from '~/features/posts/queries'
-import { useGetUser } from '~/features/users/queries'
+import type { TableColumn, TableRow } from '@nuxt/ui'
+import { useSearchPosts, type Post } from '~/features/posts/queries'
+import { useGetUser, type User } from '~/features/users/queries'
 
 const route = useRoute()
 const userId = computed(() => parseInt(route.params.id as string))
@@ -63,5 +53,23 @@ const { data: user, isLoading: isLoadingUser } = useGetUser(userId)
 
 const searchPostsParams = computed(() => ({ userId: userId.value }))
 const { data: posts, isLoading: isLoadingPosts } = useSearchPosts(searchPostsParams)
+
+
+type TableData = Post & Pick<User, 'username'>
+
+const columns: TableColumn<TableData>[] = [
+  { accessorKey: 'id', header: 'ID' },
+  { accessorKey: 'title', header: 'Title' },
+  { accessorKey: 'username', header: 'Username' },
+  { accessorKey: 'body', header: 'Body' },
+]
+
+const postsWithUsername = computed(() => {
+  if (!posts.value) return []
+  return posts.value?.map((post) => ({
+    ...post,
+    username: user.value?.username ?? ""
+  }))
+})
 
 </script>
